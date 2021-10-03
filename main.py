1c139a396e7ed2f2e6e8880e9e3550011eef6fec
nevzatozh1 <91716248+nevzatozh1@users.noreply.github.com>
2021-10-03 18:21:06 +0000

    I have been trying to do the math.log2(10) like since friday but I just could not figure it out. Then I realized that I was not doing the import command, I feel really dumb right now.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ print("Enter number x: 10")
 print("Enter number y: 4")
 x=10
 y=4
-print("x**y=",x**y)
-print("log2(x)=3.321928095")
+print("x . y=",x**y)
+import math
+print("logarithm base 2 of 10 is: ", end="")
+print(math.log2(10))
 print("78987")
